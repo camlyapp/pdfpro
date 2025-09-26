2fcaaceb1f5eec9936e109d14d4f50eea45ccd19
Bikesh Kumar <bikeshkumar0505@gmail.com>
2025-09-26 12:35:06 +0000

    ensure editing screen has equal margin in screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <Header />
-      <main className="flex-1 container py-8">
-        <PdfEditor />
+      <main className="flex-1 flex justify-center py-8 px-4">
+        <div className="w-full max-w-screen-xl">
+          <PdfEditor />
+        </div>
       </main>
     </div>
   );
